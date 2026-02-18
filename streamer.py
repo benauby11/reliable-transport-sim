92fb7e2c131f009105e5d06efa67bd188be1c186
Penny Orwant <penny@dhcp-10-105-17-27.wireless.northwestern.private>
2026-02-17 18:12:40 -0600

    175 LOC

--- streamer.py
+++ streamer.py
@@ -9,19 +9,21 @@ import hashlib
 import threading
 
 UDP_SIZE = 1472
-PACK_FORMAT = 'ii??16s'
+PREHASH_FORMAT = 'ii??'
+PACK_FORMAT = PREHASH_FORMAT + '16s' # includes hash
 HEADER_SIZE = struct.calcsize(PACK_FORMAT)
 MAX_PAYLOAD_SIZE = UDP_SIZE - HEADER_SIZE
-TIMEOUT = 0.25
+TIMEOUT = 0.1
 
 class Packet:
-    def __init__(self, seq_num = 0, payload_size = 0, is_ack = 0, is_fin = 0, payload = b''):
+    def __init__(self, seq_num = 0, payload_size = 0, is_ack = 0, is_fin = 0, payload = b'', raw_data = b''):
         self.seq_num = seq_num
         self.is_ack = is_ack
         self.is_fin = is_fin
         self.payload = payload
         self.payload_size = payload_size
-        self.hashed = hashlib.md5(self.payload).digest()
+        prehash = struct.pack(PREHASH_FORMAT, self.seq_num, self.payload_size, self.is_ack, self.is_fin)
+        self.hashed = hashlib.md5(prehash + self.payload).digest()
         self.to_send = (struct.pack(PACK_FORMAT,
                                     self.seq_num,
                                     self.payload_size,
@@ -29,20 +31,20 @@ class Packet:
                                     self.is_fin,
                                     self.hashed)
                         + self.payload)
-
-    # populate fields with unpacked raw bytes data received from socket
-    def unpack(self, data):
-        header = struct.unpack(PACK_FORMAT, data[:HEADER_SIZE])
-        self.seq_num = header[0]
-        self.payload_size = header[1]
-        self.is_ack = header[2]
-        self.is_fin = header[3]
-        self.hashed = header[4]
-        self.payload = data[HEADER_SIZE:]
+        if raw_data: # unpack the raw data to populate fields
+            header = struct.unpack(PACK_FORMAT, raw_data[:HEADER_SIZE])
+            self.seq_num = header[0]
+            self.payload_size = header[1]
+            self.is_ack = header[2]
+            self.is_fin = header[3]
+            self.hashed = header[4]
+            self.payload = raw_data[HEADER_SIZE:]
+            self.to_send = raw_data
 
     # return True if the packet has been corrupted
     def is_corrupted(self):
-        return self.hashed != hashlib.md5(self.payload).digest()
+        prehash = struct.pack(PREHASH_FORMAT, self.seq_num, self.payload_size, self.is_ack, self.is_fin)
+        return self.hashed != hashlib.md5(prehash + self.payload).digest()
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
@@ -56,12 +58,14 @@ class Streamer:
 
         self.next_to_send = 0
         self.next_to_receive = 0
-        self.last_acked = 0
+        self.last_acked = -1
 
         self.rec_buf = {}
         self.resend_buf = {}
 
         self.closed = False
+        self.fin_ack = False
+        self.fin = False
 
         self.expires = time.perf_counter() + TIMEOUT
 
@@ -75,10 +79,8 @@ class Streamer:
             try:
                 # if timeout occurs, resend packets that have not been acknowledged
                 if time.perf_counter() >= self.expires:
-                    print("TIMER EXPIRED")
-                    print(self.resend_buf.values())
-                    # lock so that shared buffer is not modified
                     self.lock.acquire()
+                    # lock so that shared buffer is not modified
                     for packet in list(self.resend_buf.values()):
                         self.socket.sendto(packet.to_send, (self.dst_ip, self.dst_port))
                     self.expires = time.perf_counter() + TIMEOUT
@@ -91,29 +93,32 @@ class Streamer:
         while not self.closed:
             try:
                 data, addr = self.socket.recvfrom()
+                self.lock.acquire()
                 if not data: # ignore
+                    self.lock.release()
                     continue
-                packet = Packet()
-                packet.unpack(data)
-                print("Received packet " + str(packet.seq_num))
+                packet = Packet(raw_data=data)
+                # drop corrupted packets
+                if packet.is_corrupted():
+                    self.lock.release()
+                    continue
 
-                if packet.is_ack: # everything up to the ack seq # has been received
-                    # lock to modify send buffer
-                    self.lock.acquire()
+                if packet.is_fin and packet.is_ack:
+                    self.fin_ack = True
+                elif packet.is_fin: # send fin ack
+                    self.fin = True
+                    fin_ack = Packet(is_ack=1, is_fin=1)
+                    self.socket.sendto(fin_ack.to_send, (self.dst_ip, self.dst_port))
+                elif packet.is_ack: # everything up to the ack seq # has been received
                     i = self.last_acked + 1
                     self.last_acked = max(self.last_acked, packet.seq_num)
                     # Remove acknowledged packets from send buffer
                     while i <= self.last_acked:
                         self.resend_buf.pop(i)
                         i += 1
-                    self.lock.release()
                 else: # add to receive buffer
-                    # drop corrupted packets
-                    if packet.is_corrupted():
-                        continue
-                    self.lock.acquire()
                     self.rec_buf[packet.seq_num] = packet
-                    self.lock.release()
+                self.lock.release()
             except Exception as e:
                 print("listener died!")
                 print(e)
@@ -130,7 +135,6 @@ class Streamer:
             self.socket.sendto(packet.to_send, (self.dst_ip, self.dst_port))
             data_bytes = data_bytes[payload_size:]
             total_len -= payload_size
-            self.expires = time.perf_counter() + TIMEOUT
 
     def recv(self) -> bytes:
         while self.rec_buf.get(self.next_to_receive) is None:
@@ -145,7 +149,7 @@ class Streamer:
             self.next_to_receive += 1
         # send acknowledgements for non-contiguous packets
         for out_of_order in list(self.rec_buf.keys()):
-            ack = Packet(seq_num=self.next_to_receive, is_ack=1)
+            ack = Packet(seq_num=self.next_to_receive - 1, is_ack=1)
             self.socket.sendto(ack.to_send, (self.dst_ip, self.dst_port))
         self.rec_buf = {}
         self.lock.release()
@@ -155,18 +159,17 @@ class Streamer:
         """Cleans up. It should block (wait) until the Streamer is done with all
            the necessary ACKs and retransmissions"""
         # your code goes here, especially after you add ACKs and retransmissions.
-        print("CLOSING")
-        fin_header = struct.pack(PACK_FORMAT, self.rec_num, 0, 0, 1, b'')
-        self.socket.sendto(fin_header, (self.dst_ip, self.dst_port))
+        while self.last_acked != self.next_to_send - 1:
+            time.sleep(0.01)
+        fin = Packet(is_fin=1)
+        self.socket.sendto(fin.to_send, (self.dst_ip, self.dst_port))
         send_time = time.perf_counter()
-        while not self.fin_ack:
+        while not (self.fin_ack or self.fin):
             if time.perf_counter() - 0.25 >= send_time:
-                self.socket.sendto(fin_header, (self.dst_ip, self.dst_port))
+                self.socket.sendto(fin.to_send, (self.dst_ip, self.dst_port))
                 send_time = time.perf_counter()
                 continue
             time.sleep(0.01)
-        print("GOT FIN ACK")
-        self.fin_ack = False
         time.sleep(2)
         self.closed = True
         self.socket.stoprecv()
